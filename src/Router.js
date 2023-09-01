9d3dcc0fb7736fa2eed063de4f8474c50fa8ceb4
권기완 <digh7169@gmail.com>
2023-09-01 15:05:37 +0900

    init commit

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/pages/Login/Login'
+import Main from './components/pages/Main/Main'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/components/pages/Login/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.scss";
+
+const Login = () => {
+
+  return (
+    <div className="login">
+      로그인 페이지
+    </div>
+  );
+};
+
+export default Login;
+
+
