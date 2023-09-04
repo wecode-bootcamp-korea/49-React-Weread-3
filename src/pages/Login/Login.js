5f140a98d2af6f6124722472577fe2a8a6ea22d5
sseyeon2 <sseyoun2@gmail.com>
2023-09-04 19:32:47 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+    //ID/PW값
+    const saveUserId = (event) => {
+        setId(event.target.value);
+    };
+    const saveUserPw = (event) => {
+        setPw(event.target.value);
+    };
+
+    const isButtonEnabled = id.includes("@") && pw.length >= 5;
+
+    //경로
     const navigate = useNavigate();
 
     const goJoin = () => {
@@ -25,9 +38,19 @@ const Login = () => {
                 </div>
                 <form className="login-form">
                     <div className="input-wrapper">
-                        <input type="text" name="e-mail" placeholder="이메일" />
-                        <input type="text" name="password" placeholder="비밀번호" />
-                        <button className="btn-login" onClick={goMain}>
+                        <input
+                            type="text"
+                            name="e-mail"
+                            placeholder="이메일"
+                            onChange={saveUserId}
+                        />
+                        <input
+                            type="text"
+                            name="password"
+                            placeholder="비밀번호"
+                            onChange={saveUserPw}
+                        />
+                        <button className="btn-login" onClick={goMain} disabled={!isButtonEnabled}>
                             로그인
                         </button>
                         <Link to="/main">메인페이지로</Link>
