e2b4727bede23aba85c6ccf6d686c334af508456
권기완 <digh7169@gmail.com>
2023-09-04 13:35:32 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -9,14 +9,27 @@ const Login = () => {
   
   const [id , setId] = useState('');
   const [password , setPassword] = useState('');
+  const [color , setColor] = useState('#add8e6');
+  const [disalbe , setDisalbe] = useState(true);
 
   const idChange = () =>{
     setId(document.getElementById("email").value)
+    
+    id.includes('@') && password.length >= 5 ? setColor('#2D71F7') : setColor('#add8e6');
+    
+    id.includes('@') && password.length >= 5 ? setDisalbe(false) : setDisalbe(true);
   }
   const passwordChange = () =>{
-    setPassword(document.getElementById("password").value)
+    setPassword(document.getElementById("password").value);
+
+    id.includes('@') && password.length >= 5 ? setColor('#2D71F7') : setColor('#add8e6');
+    id.includes('@') && password.length >= 5 ? setDisalbe(false) : setDisalbe(true);
+    
   }
-  
+
+  const logIn = () =>{
+    console.log("g2g2")
+  }
 
   return (
     <div className="login">
@@ -34,7 +47,7 @@ const Login = () => {
             <input type="text" placeholder="이메일" id = "email"onChange ={idChange}></input>
             <input type="password" placeholder="비밀번호" id="password"onChange = {passwordChange}></input>
           </div>
-          <button> 로그인 </button>
+          <button onClick = {logIn} id = "loginBtn" style = {{backgroundColor : color}} disabled={disalbe}> 로그인 </button>
           <div className = "loginMenu">
             <Link to = "/signUp" className = "borderR">회원 가입</Link>
             <a href="" >비밀번호 찾기</a>
